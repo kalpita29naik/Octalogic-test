d339dbd44876bf8cc9eae055efc07140aca66f67
Kalpita <kalpita03naik@gmail.com>
2025-08-21 00:39:31 +0530

    added data in DB

---
+++ db/seed.sql
@@ -0,0 +1,25 @@
+INSERT INTO vehicle_types (type_name, wheels) VALUES 
+('Hatchback', 4),
+('SUV', 4),
+('Sedan', 4),
+('Cruiser', 2);
+
+INSERT INTO vehicle (VName, VTypeID) VALUES 
+('Maruti Suzuki Swift', 1),
+('Hyundai Grand i10', 1),
+('Tata Tiago', 1);
+
+INSERT INTO vehicle (VName, VTypeID) VALUES 
+('Toyota Innova Crysta', 2),
+('Mahindra XUV700', 2),
+('Hyundai Creta', 2);
+
+INSERT INTO vehicle (VName, VTypeID) VALUES 
+('Maruti Suzuki Dzire', 3),
+('Hyundai Verna', 3),
+('Honda City', 3);
+
+INSERT INTO vehicle (VName, VTypeID) VALUES 
+('Royal Enfield Meteor 350', 4),
+('Jawa 42 Bobber', 4),
+('Royal Enfield Bullet 350', 4);
