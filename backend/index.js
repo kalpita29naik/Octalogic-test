fb25696977e848248c8e1fbd2305e871a1f762a2
Kalpita <kalpita03naik@gmail.com>
2025-08-22 00:35:12 +0530

    integrated booking with backend and database

--- backend/index.js
+++ backend/index.js
@@ -19,97 +19,97 @@ app.use(cors());
 //routes 
 // after wheel selection disply types of cars
 app.get('/vehicle-types', async (req, res) => {
- try {
-  const wheels = parseInt(req.query.wheels);
+  try {
+    const wheels = parseInt(req.query.wheels);
 
-  if (![2, 4].includes(wheels)) {
-   return res.status(400).json({ error: "invalid whhels" });
+    if (![2, 4].includes(wheels)) {
+      return res.status(400).json({ error: "invalid whhels" });
+    }
+
+    const types = await VehicleType.findAll({ where: { wheels } });
+    res.json(types);
   }
-
-  const types = await VehicleType.findAll({ where: { wheels } });
-  res.json(types);
- }
- catch (err) {
-  console.log(err);
-  res.status(500).json({ error: 'server error' });
- }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'server error' });
+  }
 
 })
 
 //based on car type , show car models
 app.get('/vehicles', async (req, res) => {
- try {
-  const typeId = parseInt(req.query.typeId);
+  try {
+    const typeId = parseInt(req.query.typeId);
 
-  if (!typeId) {
-   return res.status(400).json({ error: "vehicle type id is required" });
+    if (isNaN(typeId)) {
+      return res.status(400).json({ error: "vehicle type id is required" });
+    }
+
+    const vehicles = await Vehicles.findAll({
+      where: { VTypeID: typeId }
+    })
+    res.json(vehicles);
   }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "server error" })
 
-  const vehicles = await Vehicles.findAll({
-   where: { VTypeID: typeId }
-  })
-  res.json(vehicles);
- }
- catch (err) {
-  console.log(err);
-  res.status(500).json({ error: "server error" })
-
- }
+  }
 })
 
 app.post('/booking', async (req, res) => {
- try {
-  const { fname, lname, vehicleId, w, sdate, edate } = req.body;
+  try {
+    const { fname, lname, vehicleId, w, sdate, edate } = req.body;
 
-  // check if all fields are complete
-  if (!fname || !lname || !w || !sdate || !edate) {
-   return res.status(400).json({ error: "all fields are not complete" })
+    // check if all fields are complete
+    if (!fname || !lname || !w || !sdate || !edate) {
+      return res.status(400).json({ error: "all fields are not complete" })
+    }
+
+    // check if uesr exits , else create if
+    let user = await User.findOne({ where: { first_name: fname, last_name: lname } });
+
+    if (user) {
+      console.log("User exits")
+    }
+    else {
+      console.log("User does not exists, so add detils of new user")
+      user = await User.create({ first_name: fname, last_name: lname })
+    }
+
+    // check for dates and same vehicle
+    const matchingDate = await Booking.findOne({
+      where: {
+        VID: vehicleId,
+        [Op.or]: [
+          {
+            start_date: { [Op.between]: [sdate, edate] }
+          },
+          {
+            end_date: { [Op.between]: [sdate, edate] }
+          }
+        ]
+      }
+    });
+
+    if (matchingDate) {
+      return res.status(400).json({ error: "Vehicle already booked" });
+    }
+
+    // if everything is fine , thenbook the vehicle
+    const booking = await Booking.create({
+      userID: user.userID,
+      VID: vehicleId,
+      start_date: sdate,
+      end_date: edate
+    });
+    res.json({ message: "Booking confirmed!", booking });
+
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
   }
-
-  // check if uesr exits , else create if
-  let user = await User.findOne({ where: { first_name: fname, last_name: lname } });
-
-  if (user) {
-   console.log("User exits")
-  }
-  else {
-   console.log("User does not exists, so add detils of new user")
-   user = await User.create({ first_name: fname, last_name: lname })
-  }
-
-  // check for dates and same vehicle
-  const matchingDate = await Booking.findOne({
-   where: {
-    VID: vehicleId,
-    [Op.or]: [
-     {
-      start_date: { [Op.between]: [sdate, edate] }
-     },
-     {
-      end_date: { [Op.between]: [sdate, edate] }
-     }
-    ]
-   }
-  });
-
-  if (matchingDate) {
-   return res.status(400).json({ error: "Vehicle already booked" });
-  }
-
-  // if everything is fine , thenbook the vehicle
-  const booking = await Booking.create({
-   userID: user.userID,
-   VID: vehicleId,
-   start_date: sdate,
-   end_date: edate
-  });
-   res.json({ message: "Booking confirmed!", booking });
-
-
- } catch (err) {
-  console.error(err);
-  res.status(500).json({ error: "Server error" });
- }
 
 })
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,240 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Button, FormControlLabel, Radio, RadioGroup, TextField } from "@mui/material"
+import { useEffect } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    wheels: "",
+    vehicleType: "",
+    vehicle: "",
+    startDate: "",
+    endDate: ""
+  });
+
+  const [step, setStep] = useState(1);
+  const [vehicleTypes, setVehiclesTypes] = useState([]);
+  const [vehiclesName, setVehiclesName] = useState([]);
+
+  useEffect(() => {
+    if (step === 3 && formData.wheels) {
+      fetch(`http://localhost:5000/vehicle-types?wheels=${formData.wheels}`)
+        .then(res => res.json())
+        .then(data => setVehiclesTypes(data))
+        .catch(err => console.log(err));
+    }
+
+  }, [step, formData.wheels]);
+
+  // useEffect(() => {
+  //   if (step === 4 && formData.vehicleType) {
+  //     fetch(`http://localhost:5000/vehicles?typeId=${formData.vehicleType}`)
+  //       .then(res => res.json())
+  //       .then(data => setVehiclesName(data))
+  //       .catch(err => console.log(err));
+  //   }
+  // }, [step, formData.vehicleType]);
+
+  useEffect(() => {
+    if (step === 4 && formData.vehicleType) {
+      fetch(`http://localhost:5000/vehicles?typeId=${formData.vehicleType}`)
+        .then(res => res.json())
+        .then(data => {
+          console.log("Vehicles API response:", data);
+          setVehiclesName(Array.isArray(data) ? data : []);
+        })
+        .catch(err => console.log(err));
+    }
+  }, [step, formData.vehicleType]);
+
+
+
+  const handleNext = async () => {
+    switch (step) {
+      case 1:
+        if (!formData.firstName || !formData.lastName) return alert("Enter your name");
+        break;
+
+      case 2:
+        if (!formData.wheels) return alert("Select your choice of wheels");
+        break;
+
+      case 3:
+        if (!formData.vehicleType) return alert("Select your choice of Vehicles");
+        break;
+
+      case 4:
+        if (!formData.vehicle) return alert("Select your choice of Vehicle Model");
+        break;
+
+      case 5:
+        if (!formData.startDate || !formData.endDate) return alert("Select Dates");
+
+        console.log("Selected VehicleType:", formData.vehicleType);
+        console.log("Selected Vehicle:", formData.vehicle);
+
+
+        try {
+          const res = await fetch("http://localhost:5000/booking", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              fname: formData.firstName,
+              lname: formData.lastName,
+              vehicleId: Number(formData.vehicle),
+              w: Number(formData.wheels),
+              sdate: formData.startDate,
+              edate: formData.endDate,
+            })
+
+          });
+
+          const result = await res.json();
+
+          console.log("Selected vehicleType:", formData.vehicleType);
+          console.log("Available vehicleTypes:", vehicleTypes);
+
+          if (res.ok) {
+            alert("Booking sucessfull");
+            console.log(result);
+          } else {
+            alert(result.error || "Bookign failed");
+          }
+
+        } catch (err) {
+          console.log(err);
+          alert("Server err")
+        }
+        return;
+
+
+      default: break;
+
+    }
+    setStep(step + 1)
+  }
   return (
-    <div className='bg-red-500'>
-      Car rental system
-    </div>
+    <div className='flex justify-center items-center min-h-screen bg-[#F2F9FF]'>
+      <div className='p-6 rounded-lg shadow-blue-200 shadow-xl w-96'>
+
+        {step === 1 && (
+          <div>
+            <h1 className='text-3xl'>First, What's Your Name?</h1>
+            {/* first name div */}
+            <div className='flex flex-col mt-6'>
+              <label>First Name</label>
+              <TextField
+                value={formData.firstName}
+                onChange={(e) => setFormData({ ...formData, firstName: e.target.value })}
+                size='small'
+              />
+            </div>
+
+            {/* last name div */}
+            <div className='flex flex-col mt-6'>
+              <label>Last Name</label>
+              <TextField
+                value={formData.lastName}
+                onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+                size='small'
+              />
+            </div>
+          </div>
+        )}
+
+        {step === 2 && (
+          <div>
+            <h1 className='text-3xl'>Select Number of Wheels</h1>
+            <RadioGroup
+              value={formData.wheels}
+              onChange={(e) => setFormData({ ...formData, wheels: e.target.value })}
+              className='mt-3'
+            >
+              <FormControlLabel label="4" control={<Radio />} value={4} />
+              <FormControlLabel label="2" control={<Radio />} value={2} />
+            </RadioGroup>
+          </div>
+        )}
+
+        {step === 3 && (
+          <div>
+            <h1 className='text-3xl'> Select Type of Vehicle</h1>
+            <RadioGroup
+              value={formData.vehicleType}
+              onChange={(e) => setFormData({ ...formData, vehicleType: e.target.value })}
+            >
+              {vehicleTypes.map((type) => (
+                <FormControlLabel
+                  key={type.VTypeID}
+                  value={String(type.VTypeID)}  // pass the actual typeId
+                  control={<Radio />}
+                  label={type.type_name}
+                />
+              ))}
+            </RadioGroup>
+          </div>
+        )}
+
+        {step === 4 && (
+          <div>
+            <h1 className="text-3xl">Select Model of Vehicle</h1>
+            <RadioGroup
+              value={formData.vehicle}
+              onChange={(e) => setFormData({ ...formData, vehicle: e.target.value })}
+            >
+              {vehiclesName.map((type) => (
+                <FormControlLabel
+                  key={type.VID}
+                  value={String(type.VID)}
+                  control={<Radio />}
+                  label={type.VName}
+                />
+              ))}
+            </RadioGroup>
+
+          </div>
+        )}
+
+        {step === 5 && (
+          <div>
+            <h1 className='text-3xl'>Select Your Dates</h1>
+            <div className='flex flex-col mt-6'>
+              <label>Start Date</label>
+              <TextField
+                type='date'
+                value={formData.startDate}
+                onChange={(e) => setFormData({ ...formData, startDate: e.target.value })}
+
+              />
+            </div>
+
+            <div className='flex flex-col mt-6'>
+              <label>End Date</label>
+              <TextField
+                type='date'
+                value={formData.endDate}
+                onChange={(e) => setFormData({ ...formData, endDate: e.target.value })}
+
+              />
+            </div>
+
+
+
+
+
+
+
+          </div>
+        )}
+
+        {/* button */}
+        <div onClick={handleNext} className='mt-6'>
+          <Button variant='contained' className='w-full'>{step === 5 ? "Submit" : "Next"}</Button>
+        </div>
+      </div>
+    </div >
   );
 }
 
